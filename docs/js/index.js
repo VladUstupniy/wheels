dc9fe88e39d77921889cb11f5b31f20f29ef4b02
Vlad <vlad.ustupniy@yandex.ru>
2023-03-19 16:25:36 +0300

    new blocks

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,5 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('menu').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('menu').addEventListener('click', () => {
         document.querySelector('header').classList.toggle('open');
     });
+
+    const dropDownLink = document.getElementById('drop-down__link');
+    const dropDownContent = document.getElementById('drop-down__content');
+    dropDownLink.addEventListener('click', () => {
+        dropDownLink.classList.toggle('active');
+        dropDownContent.classList.toggle('active');
+    });
+
 });
